6159c833cb56f9d58f5a52f62f183c0656f7199b
Burhan Uddin <bunnub5683@gmail.com>
2022-06-14 19:42:53 +0600

    section title fixed

--- pages/Main/SectionTitle.js
+++ pages/Main/SectionTitle.js
@@ -5,8 +5,10 @@ const SectionTitle = ({ children }) => {
     return (
         <div className="container mx-auto px-8" data-aos="zoom-in">
             <div className='section-title text-center z-10'>
-                <h2>{children}</h2>
-                <h3 className='hidden lg:block text-[50px] lg:text-[96px]'>{children}</h3>
+                <h2 className='text-[15px] lg:text-[32px]'>{children}</h2>
+                <h3 className='lg:block text-[32px] lg:text-[96px]'>{children}</h3>
+            </div>
+            <div className='section-title text-center z-10'>
             </div>
         </div>
     );
