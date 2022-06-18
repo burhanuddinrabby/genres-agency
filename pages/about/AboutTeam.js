2d23937bd105b13659835e8468c64ab01d2b3ae9
Burhan Uddin <bunnub5683@gmail.com>
2022-06-18 17:40:26 +0600

    about us changed

--- pages/about/AboutTeam.js
+++ pages/about/AboutTeam.js
@@ -7,8 +7,8 @@ const AboutTeam = () => {
             <h1 className='text-primary font-semibold text-2xl'>
                 Meet With Our Team
             </h1>
-            <p className='text-[#8E8E8E] text-left mt-12 mb-24 text-xl'>
-                
+            <p className='text-[#8E8E8E] text-center mt-12 mb-24 text-xl'>
+
                 <blockquote>
                     It&apos;s dusk, so dark. <br />
 
@@ -34,24 +34,20 @@ const AboutTeam = () => {
                 And you may or may not have resources or talents.<br />
 
                 But you want agency (quick actions). Only actions generate results, you know it.
-
                 So it&apos;s tempting to try to find the best of talents spending long delays and big-bucks―especially the money we get for free (perceptually-free: you still have to return profits to investors) or time that we take for granted.
-
                 Also it&apos;s a little challenging to find talents who are willing to work in a brand new startup.
-
                 And a Digital Agency can help you here. Agency is a business that accumulates talents, skills, insider-knowledge and makes it cost-effective for others to use.
-
                 We tried to be a startup first, we lingered for a year and two months, couldn&apos;t survive, so quit for some time.
-
                 This means we understand your pains. So we can help you best.
-
                 What do you need to turn an idea into an actual product/service!?
 
-                - Product design (UI/UX)
-                - Website/app development
-                - Digital marketing
-                - Content writing
-                - Management/Consultancy
+                <p className="text-primary">
+                    - Product design (UI/UX) <br />
+                    - Website/app development <br />
+                    - Digital marketing <br />
+                    - Content writing <br />
+                    - Management/Consultancy <br />
+                </p>
 
                 All 5 you get here in GenRes Agency, except the app development, for now. It&apos;s not costly.
 
@@ -62,8 +58,9 @@ const AboutTeam = () => {
                 But you should not trust my words.
 
                 You can create a custom package based on your needs and see our works.
-
-                [&apos;Create a package&apos; button]
+                <button className='btn rounded-none px-12 bg-gradient-to-r from-primary to-secondary text-[#fff] border-none mx-auto block lg:mt-24 mt-5'>
+                    Create a package
+                </button>
 
             </p>
         </div>
