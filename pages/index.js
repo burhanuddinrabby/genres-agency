6453e5a00ac4ebe5026a871b605de2748ae7e4ee
Burhan Uddin <bunnub5683@gmail.com>
2022-06-19 00:00:44 +0600

    testing preview image

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,20 @@ export default function Home() {
     <div>
       <Head>
         <title>HOME | GenRes</title>
-        <meta property="og:title" content="genres"/>
-        <meta property="og:description" content="we quickly generate your crucial results"/>
-        <meta property="og:image" content="https://i.ibb.co/FWSpk8G/preview.png"/>
-        <meta property="twitter:card" content="summary_large_image"/>
+        {/* <meta property="og:title" content="GenRes" />
+        <meta property="og:description" content="we quickly generate your crucial results" />
+        <meta property="og:image" content="https://i.ibb.co/FWSpk8G/preview.png" />
+        <meta property="twitter:card" content="summary_large_image" /> */}
+
+        {/* <meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1"/> */}
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="og:image:type" content="image/png" />
+        <meta data-n-head="ssr" data-hid="og:locale" name="og:locale" content="en_US" />
+        <meta property="og:url" content="https://genres-agency.vercel.app/" />
+        <meta data-n-head="ssr" data-hid="og:image" name="og:image"
+          content="https://i.ibb.co/FWSpk8G/preview.png" />
+        <meta data-n-head="ssr" data-hid="og:title" name="og:title" content="Truth or Dare Bot" />
+        <meta property="og:description" />
       </Head>
       <Navbar></Navbar>
       <Banner></Banner>
