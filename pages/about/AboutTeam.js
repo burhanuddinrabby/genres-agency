dee7b5c00f3f6eb84c457f18e9671cc534ae6015
Burhan Uddin <bunnub5683@gmail.com>
2022-06-18 18:36:02 +0600

    about us done

--- pages/about/AboutTeam.js
+++ pages/about/AboutTeam.js
@@ -5,9 +5,12 @@ const AboutTeam = () => {
     return (
         <div className='text-center'>
             <h1 className='text-primary font-semibold text-2xl'>
-                Meet With Our Team
+                About Us
             </h1>
-            <p className='text-[#8E8E8E] text-center mt-12 mb-24 text-xl'>
+            {/* <h1 className='text-primary font-semibold text-2xl'>
+                Meet With Our Team
+            </h1> */}
+            <p className='text-[#8E8E8E] text-left mt-12 mb-24 text-xl'>
 
                 <blockquote>
                     It&apos;s dusk, so dark. <br />
@@ -18,7 +21,7 @@ const AboutTeam = () => {
 
                     Chanakya is working on a few papers under the oil lamp (old times!). He welcomes him smiling, and asks to sit. He then hurries his work, turns off the lamp, lights another. <br />
 
-                    This was strange. And if you are like me or the traveler, you would be tensed to learn why he did this. <br />
+                    This was strange. And if you are like me or the traveler, you would be tensed to learn why he did this. <br /><br />
 
                     So the traveler could no longer control: <span className='font-semibold'>&quot;Is this an Indian custom?&quot;</span><br />
 
@@ -26,20 +29,26 @@ const AboutTeam = () => {
 
                     : <span className='font-semibold'>&quot;To light up another lamp when a guest visits!?&quot;</span><br />
 
-                    - <span className='font-semibold'>&quot;No, my dear friend. There is no such custom. I was doing my nation&apos;s official work when you entered and the lamp&apos;s oil was from National treasure&apos;s money. Now I&apos;m talking with you, it&apos;s a personal and friendly conversation, so I&apos;m using oil purchased from personal money.&quot;</span><br />
+                    - <span className='font-semibold'>&quot;No, my dear friend. There is no such custom. I was doing my nation&apos;s official work when you entered and the lamp&apos;s oil was from National treasure&apos;s money. <br />
+                        Now I&apos;m talking with you, it&apos;s a personal and friendly conversation, so I&apos;m using oil purchased from personal money.&quot;</span><br /><br />
                 </blockquote>
 
                 Agency means being in the state of action. When you are a new startup, either you have massive funds (from investors or bootstrapped) or you have zero.<br />
 
                 And you may or may not have resources or talents.<br />
 
-                But you want agency (quick actions). Only actions generate results, you know it. <br />
-                So it&apos;s tempting to try to find the best of talents spending long delays and big-bucks―especially the money we get for free (perceptually-free: you still have to return profits to investors) or time that we take for granted. <br />
+                But you want agency (quick actions). Only actions generate results, you know it. <br /><br />
+
+                So it&apos;s tempting to try to find the best of talents spending long delays and big-bucks―especially the money we get for free (perceptually-free: you still have to return profits to investors) or time that we take for granted. <br /><br />
+
                 Also it&apos;s a little challenging to find talents who are willing to work in a brand new startup. <br />
-                And a Digital Agency can help you here. Agency is a business that accumulates talents, skills, insider-knowledge and makes it cost-effective for others to use.
-                We tried to be a startup first, we lingered for a year and two months, couldn&apos;t survive, so quit for some time.
-                This means we understand your pains. So we can help you best.
-                What do you need to turn an idea into an actual product/service!?
+
+                And a Digital Agency can help you here. Agency is a business that accumulates talents, skills, insider-knowledge and makes it cost-effective for others to use. <br /><br />
+
+                We tried to be a startup first, we lingered for a year and two months, couldn&apos;t survive, so quit for some time. <br />
+                This means we understand your pains. So we can help you best. <br /><br />
+
+                What do you need to turn an idea into an actual product/service!? <br />
 
                 <p className="text-primary">
                     - Product design (UI/UX) <br />
@@ -47,23 +56,25 @@ const AboutTeam = () => {
                     - Digital marketing <br />
                     - Content writing <br />
                     - Management/Consultancy <br />
-                </p>
+                </p><br /><br />
 
-                All 5 you get here in GenRes Agency, except the app development, for now. It&apos;s not costly.
+                All 5 you get here in GenRes Agency, except the app development, for now. <br />
 
-                It&apos;s also fast.
+                <p className="text-primary">
+                    - It&apos;s not costly. <br />
+                    - It is also fast. <br />
+                    - It is reliable. <br />
+                </p><br />
 
-                It is reliable.
-
-                But you should not trust my words.
+                But you should not trust my words. <br /><br />
 
                 You can create a custom package based on your needs and see our works.
-                <button className='btn rounded-none px-12 bg-gradient-to-r from-primary to-secondary text-[#fff] border-none mx-auto block lg:mt-24 mt-5'>
+                <button className='btn rounded-none px-12 bg-gradient-to-r from-primary to-secondary text-[#fff] border-none block lg:mt-20 mt-5'>
                     Create a package
                 </button>
 
             </p>
-        </div>
+        </div >
     );
 };
 
