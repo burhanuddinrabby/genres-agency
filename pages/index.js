0a8e17aa7e327c913c5fd0215958ce95da1b043f
Burhan Uddin <bunnub5683@gmail.com>
2022-06-19 00:26:48 +0600

    preview image done

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,31 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>HOME | GenRes</title>
-        {/* <meta property="og:title" content="GenRes" />
-        <meta property="og:description" content="we quickly generate your crucial results" />
-        <meta property="og:image" content="https://i.ibb.co/FWSpk8G/preview.png" />
-        <meta property="twitter:card" content="summary_large_image" /> */}
-
-        {/* <meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1"/> */}
-        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="og:title" content="GenRes" />
         <meta property="og:image:type" content="image/png" />
-        <meta data-n-head="ssr" data-hid="og:locale" name="og:locale" content="en_US" />
         <meta property="og:url" content="https://genres-agency.vercel.app/" />
-        <meta data-n-head="ssr" data-hid="og:image" name="og:image"
-          content="https://i.ibb.co/FWSpk8G/preview.png" />
-        <meta data-n-head="ssr" data-hid="og:title" name="og:title" content="Truth or Dare Bot" />
-        <meta property="og:description" />
+        <meta data-n-head="ssr" data-hid="og:image" name="og:image" content="https://i.ibb.co/FWSpk8G/preview.png" />
+        <meta data-n-head="ssr" data-hid="og:locale" name="og:locale" content="en_US" />
+        <meta property="og:description" content="We quickly generate your crucial results" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Home | GenRes</title>
+        <meta name="title" content="GenRes" />
+        <meta name="description" content="We quickly generate your crucial results" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://genres-agency.vercel.app/" />
+        <meta property="og:title" content="GenRes" />
+        <meta property="og:description" content="We quickly generate your crucial results" />
+        <meta property="og:image" content="https://i.ibb.co/FWSpk8G/preview.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://genres-agency.vercel.app/" />
+        <meta property="twitter:title" content="GenRes" />
+        <meta property="twitter:description" content="We quickly generate your crucial results" />
+        <meta property="twitter:image" content="https://i.ibb.co/FWSpk8G/preview.png" />
       </Head>
       <Navbar></Navbar>
       <Banner></Banner>
