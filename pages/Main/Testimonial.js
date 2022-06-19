26bbbc898c1e964abe339d12ab419ec74d99cd55
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-06-19 08:51:43 +0600

    Contact page done

--- pages/Main/Testimonial.js
+++ pages/Main/Testimonial.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from './SectionTitle';
-import Image from 'next/image';
-import man from '../../public/image/man.jpg'
-import woman from '../../public/image/woman.jpg'
-import quote from '../../public/image/quote.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeftLong, faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "./SectionTitle";
+import Image from "next/image";
+import man from "../../public/image/man.jpg";
+import woman from "../../public/image/woman.jpg";
+import quote from "../../public/image/quote.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeftLong,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 //
 // const Testimonial = () => {
@@ -104,46 +107,58 @@ import '@splidejs/react-splide/css';
 //     );
 // };
 
-
 const testimonials_list = [
-    {
-        image: man,
-        messege: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
-        author: "Mujahid"
-    },
-    {
-        image: man,
-        messege: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
-        author: "Shajedul Karim"
-    }
-]
+  {
+    id: 1,
+    image: man,
+    messege:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    author: "Mujahid",
+  },
+  {
+    id: 2,
+    image: man,
+    messege:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    author: "Shajedul Karim",
+  },
+  {
+    id: 3,
+    image: man,
+    messege:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    author: "Saidur Rahman",
+  },
+];
 
 const Testimonial = () => {
+  return (
+    <div className="lg:my-36 my-20">
+      <SectionTitle>Testimonial</SectionTitle>
+      <p
+        className="w-full mx-auto text-[#8e8e8e] lg:mt-16 mt-6 text-center"
+        data-aos="fade-up"
+      >
+        {/* Our clients are not satisfied. */}
+        Clients we&apos;ve worked with
+      </p>
 
-    return (
-        <div className='lg:my-36 my-20'>
-            <SectionTitle>
-                Testimonial
-            </SectionTitle>
-            <p className='w-full mx-auto text-[#8e8e8e] lg:mt-16 mt-6 text-center' data-aos="fade-up">
-                {/* Our clients are not satisfied. */}
-                Clients we&apos;ve worked with
-            </p>
+      <div className="lg:px-48 px-2 lg:mt-24 mt-10" data-aos="fade-up">
+        <div className="text-center">
+          <em className="text-2xl">
+            We still have <b className="text-primary">zero</b> client. <br />
+            This means we are available for{" "}
+            <b className="text-primary"> you immediately</b>.
+            <br />
+            We are a <b className="text-primary">brand new</b> Agency, but each
+            team member is skilled in their fields. <br />
+            Looking forward to work with you.
+          </em>
+        </div>
 
-            <div className="lg:px-48 px-2 lg:mt-24 mt-10" data-aos="fade-up">
-                <div className='text-center'>
-                    <em className='text-2xl'>
-                        We still have <b className='text-primary'>zero</b> client. <br />
-                        This means we are available for <b className="text-primary"> you immediately</b>.
-                        <br />
-                        We are a <b className="text-primary">brand new</b> Agency, but each team member is skilled in their fields. <br />
-                        Looking forward to work with you. 
-                    </em>
-                </div>
+        {/* [keep this simple but emphasized [bold or increased font], no need to use box/borders] */}
 
-                {/* [keep this simple but emphasized [bold or increased font], no need to use box/borders] */}
-
-                {/* <Splide hasTrack={ false } options={ {   type: 'loop', drag:true, focus: 'center' } } aria-label={"Testimonial"}>
+        {/* <Splide hasTrack={ false } options={ {   type: 'loop', drag:true, focus: 'center' } } aria-label={"Testimonial"}>
                     <SplideTrack>
                     {testimonials_list.map((item, index)=>(
                         <SplideSlide key={index}>
@@ -166,15 +181,13 @@ const Testimonial = () => {
                     </SplideTrack>
                     //ekane nicher ta hbe
                 </Splide> */}
-                {/* <div className="splide__arrows">
+        {/* <div className="splide__arrows">
                        <button className="splide__arrow splide__arrow--prev"><FontAwesomeIcon icon="fas fa-chevron-left" /></button>
                        <button className="splide__arrow splide__arrow--next">Next</button>
                     </div> */}
-            </div>
-
-        </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default Testimonial;
--- pages/about/AboutTeam.js
+++ pages/about/AboutTeam.js
@@ -1,70 +1,78 @@
-import React from 'react';
-import SectionTitle from '../Main/SectionTitle';
+import React from "react";
+import SectionTitle from "../Main/SectionTitle";
 
 const AboutTeam = () => {
-    return (
-        <div className='text-center'>
-            <h1 className='text-primary font-semibold text-2xl'>
-                Meet With Our Team
-            </h1>
-            <p className='text-[#8E8E8E] text-center mt-12 mb-24 text-xl'>
-
-                <blockquote>
-                    It&apos;s dusk, so dark. <br />
-
-                    A traveler wants to meet Chanakya, he came from far. <br />
-
-                    He enters Chanakya&apos;s home. <br />
-
-                    Chanakya is working on a few papers under the oil lamp (old times!). He welcomes him smiling, and asks to sit. He then hurries his work, turns off the lamp, lights another. <br />
-
-                    This was strange. And if you are like me or the traveler, you would be tensed to learn why he did this. <br />
-
-                    So the traveler could no longer control: <span className='font-semibold'>&quot;Is this an Indian custom?&quot;</span><br />
-
-                    - <span className='font-semibold'>&quot;Which one?&quot;</span><br />
-
-                    : <span className='font-semibold'>&quot;To light up another lamp when a guest visits!?&quot;</span><br />
-
-                    - <span className='font-semibold'>&quot;No, my dear friend. There is no such custom. I was doing my nation&apos;s official work when you entered and the lamp&apos;s oil was from National treasure&apos;s money. Now I&apos;m talking with you, it&apos;s a personal and friendly conversation, so I&apos;m using oil purchased from personal money.&quot;</span><br />
-                </blockquote>
-
-                Agency means being in the state of action. When you are a new startup, either you have massive funds (from investors or bootstrapped) or you have zero.<br />
-
-                And you may or may not have resources or talents.<br />
-
-                But you want agency (quick actions). Only actions generate results, you know it. <br />
-                So it&apos;s tempting to try to find the best of talents spending long delays and big-bucks―especially the money we get for free (perceptually-free: you still have to return profits to investors) or time that we take for granted. <br />
-                Also it&apos;s a little challenging to find talents who are willing to work in a brand new startup. <br />
-                And a Digital Agency can help you here. Agency is a business that accumulates talents, skills, insider-knowledge and makes it cost-effective for others to use.
-                We tried to be a startup first, we lingered for a year and two months, couldn&apos;t survive, so quit for some time.
-                This means we understand your pains. So we can help you best.
-                What do you need to turn an idea into an actual product/service!?
-
-                <p className="text-primary">
-                    - Product design (UI/UX) <br />
-                    - Website/app development <br />
-                    - Digital marketing <br />
-                    - Content writing <br />
-                    - Management/Consultancy <br />
-                </p>
-
-                All 5 you get here in GenRes Agency, except the app development, for now. It&apos;s not costly.
-
-                It&apos;s also fast.
-
-                It is reliable.
-
-                But you should not trust my words.
-
-                You can create a custom package based on your needs and see our works.
-                <button className='btn rounded-none px-12 bg-gradient-to-r from-primary to-secondary text-[#fff] border-none mx-auto block lg:mt-24 mt-5'>
-                    Create a package
-                </button>
-
-            </p>
-        </div>
-    );
+  return (
+    <div className="text-center">
+      <h1 className="text-primary font-semibold text-2xl">
+        Meet With Our Team
+      </h1>
+      <p className="text-[#8E8E8E] text-left mt-12 mb-24 text-xl">
+        <blockquote>
+          It&apos;s dusk, so dark. <br />
+          A traveler wants to meet Chanakya, he came from far. <br />
+          He enters Chanakya&apos;s home. <br />
+          Chanakya is working on a few papers under the oil lamp (old times!).
+          He welcomes him smiling, and asks to sit. He then hurries his work,
+          turns off the lamp, lights another. <br />
+          This was strange. And if you are like me or the traveler, you would be
+          tensed to learn why he did this. <br />
+          So the traveler could no longer control:{" "}
+          <span className="font-semibold">
+            &quot;Is this an Indian custom?&quot;
+          </span>
+          <br />- <span className="font-semibold">&quot;Which one?&quot;</span>
+          <br />:{" "}
+          <span className="font-semibold">
+            &quot;To light up another lamp when a guest visits!?&quot;
+          </span>
+          <br />-{" "}
+          <span className="font-semibold">
+            &quot;No, my dear friend. There is no such custom. I was doing my
+            nation&apos;s official work when you entered and the lamp&apos;s oil
+            was from National treasure&apos;s money. Now I&apos;m talking with
+            you, it&apos;s a personal and friendly conversation, so I&apos;m
+            using oil purchased from personal money.&quot;
+          </span>
+          <br />
+        </blockquote>
+        Agency means being in the state of action. When you are a new startup,
+        either you have massive funds (from investors or bootstrapped) or you
+        have zero.
+        <br />
+        And you may or may not have resources or talents.
+        <br />
+        But you want agency (quick actions). Only actions generate results, you
+        know it. <br />
+        So it&apos;s tempting to try to find the best of talents spending long
+        delays and big-bucks―especially the money we get for free
+        (perceptually-free: you still have to return profits to investors) or
+        time that we take for granted. <br />
+        Also it&apos;s a little challenging to find talents who are willing to
+        work in a brand new startup. <br />
+        And a Digital Agency can help you here. Agency is a business that
+        accumulates talents, skills, insider-knowledge and makes it
+        cost-effective for others to use. We tried to be a startup first, we
+        lingered for a year and two months, couldn&apos;t survive, so quit for
+        some time. This means we understand your pains. So we can help you best.
+        What do you need to turn an idea into an actual product/service!?
+        <p className="text-primary">
+          - Product design (UI/UX) <br />
+          - Website/app development <br />
+          - Digital marketing <br />
+          - Content writing <br />
+          - Management/Consultancy <br />
+        </p>
+        All 5 you get here in GenRes Agency, except the app development, for
+        now. It&apos;s not costly. It&apos;s also fast. It is reliable. But you
+        should not trust my words. You can create a custom package based on your
+        needs and see our works.
+        <button className="btn rounded-none px-12 bg-gradient-to-r from-primary to-secondary text-[#fff] border-none mx-auto block lg:mt-24 mt-5">
+          Create a package
+        </button>
+      </p>
+    </div>
+  );
 };
 
 export default AboutTeam;
